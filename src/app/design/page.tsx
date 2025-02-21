83b69858e4371983914efd4eeeaae88c19af9504
vachuska1 <aless.vachuska@seznam.cz>
2025-02-21 16:07:01 +0100

    Nahrání celého projektu

---
+++ src/app/design/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Design() {
+  return (
+    <div>
+      <h1>Tvorba webů - Web design</h1>
+      <p>Obsah této sekce...</p>
+      <Link href="/">Zpět na úvod</Link>
+    </div>
+  );
+}
---
+++ src/app/odhady/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Odhady() {
+  return (
+    <div>
+      <h1>Oceňování nemovitých věcí</h1>
+      <p>Obsah této sekce...</p>
+      <Link href="/">Zpět na úvod</Link>
+    </div>
+  );
+}
