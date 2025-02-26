be9626cb4419c36086949e655b7b1d34d69cb64c
vachuska1 <aless.vachuska@seznam.cz>
2025-02-26 14:41:48 +0100

    Příprava na nasazení na Netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  exportTrailingSlash: true,  // Přidání konfigurace pro export
 };
 
 export default nextConfig;
